35187c8303390f6e6c0d31e9297ac1400f2db898
lotekone <joar@dedypeskoger>
2022-06-07 18:52:56 +0200

    added auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import GuideCard from './pages/GuideCard'
 import GuideDetail  from './pages/[slug]'
 import NotFound from './pages/NotFound'
-const App = () =>  (
+import Auth0ProviderWithHistory from './Auth0Provider'
+
+const App = () => (
   <Router>
+    <Auth0ProviderWithHistory>
     <Routes>
       <Route path="/" element={<GuideCard />} />
-      <Route path="/page/:slug" element={<GuideDetail />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/guide/:slug" element={<GuideDetail />} />
+      <Route path="404" element={<NotFound />} />
     </Routes>
+    </Auth0ProviderWithHistory>
   </Router>
   )
 
--- src/pages/GuideCard.jsx
+++ src/pages/GuideCard.jsx
@@ -1,45 +1,57 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import useContentful from "../useContentful";
 
+
 const GuideCard = () => {
   const [entries, setEntries] = useState([])
-  const [filter, setFilter] = useState('')
+  const [filter, setFilter] = useState("")
   const {getGuides} = useContentful()
+  const { loginWithRedirect, logout, user, isLoading } = useAuth0();
 
   useEffect(() => {
     getGuides().then(data => {
       setEntries (data)
     })
   }, [])
-  if (entries.length === 0) {
-    return <div>Loading...</div>
-  }
-  const filteredEntries = entries.filter(entry => {
-    return entry.fields.header.toLowerCase().includes(filter.toLowerCase())
-  })
+const filteredEntries = entries.filter(entry => {
+  return entry.fields.header?.toLowerCase().includes(filter.toLowerCase())
+})
 
   return (
-    <div>
-      <h1>Guides</h1>
-      <input className="search" type="text" placeholder="Search Guides" value={filter} onChange={({target}) => setFilter(target.value)}/>
+    <>
+    {!isLoading && !user && (
+   <button className="button" onClick={() => loginWithRedirect()}>
+      Login
+   </button>
+   )}
+    {!isLoading && user?.name && (
+    <> <button className="button" onClick={() => logout()}>
+      Logout
+    </button>
+    <span>Hello {user?.nickname} </span>
+    </>
+    )}
+      <input className="search" type="text" placeholder="Search Guides" value={filter} onChange={({ target }) => setFilter(target.value) } />
+      
       {filteredEntries.map((entry) => {
         const {fields} = entry
         
         return (
-  <Main key={entry.sys.id}>
-    <Wrapper background={fields.thumbnail.fields.file.url}>
-      <TextContainer>
-        <Link to={`/page/${fields.slug}`}>
-        <Title>{fields.header}</Title>
-        </Link> 
-      </TextContainer>
-    </Wrapper> 
-  </Main>
+          <Main key={entry.sys.id}>
+            <Wrapper background={fields.thumbnail.fields.file.url}>
+              <TextContainer>
+                <Link to={`/guide/${fields.slug}`}>
+                <Title>{fields.header}</Title>
+                </Link>
+              </TextContainer>
+            </Wrapper> 
+          </Main>
         )
       })}
-    </div>
+    </>
   );
 };
 
@@ -63,8 +75,8 @@ const Wrapper = styled.div`
   border-radius: 20px;
   box-shadow: 0px 20px 40px rgba(52, 53, 99, 0.2),
     0px 1px 3px rgba(0, 0, 0, 0.05);
-  background: ${(props) =>
-    props.background && `url(${props.background}) center no-repeat`};
+  background: ${({background}) =>
+    background && `url(${background}) center no-repeat`};
   background-size: auto 150%;
 `;
 
@@ -78,7 +90,7 @@ const TextContainer = styled.div`
   a:link,
   a:visited {
     text-decoration: none;
-    color: #fff;
+    color: #000;
   }
   :hover {
     height: fit-content;
@@ -101,12 +113,4 @@ const Title = styled.p`
   margin: 0px;
 `;
 
-const Subtitle = styled.p`
-  font-weight: normal;
-  font-size: 12px;
-  font-style: italic;
-  color: #fff;
-  margin: 0px;
-  visibility: hidden;
-  display: none;
-`;
+
